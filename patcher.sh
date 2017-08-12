5339f0bb279ea76ab07a4c34930861a673b2b543
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2017-08-12 20:49:24 +0600

    patcher: ace: Add NON_PIE patch

--- patcher.sh
+++ patcher.sh
@@ -7,6 +7,8 @@ PATCHER_PATH=$ROM_TREE/patcher
 ACE=$PATCHER_PATH/ace
 
 # Clean up first
+cd $ROM_TREE/bionic
+git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/opt/telephony
 git clean -f -d && git reset --hard
 
@@ -14,3 +16,4 @@ cd $ROM_TREE
 
 ### ace patches
 patch -d frameworks/opt/telephony				-p1 -s -N --no-backup-if-mismatch < $ACE/data_connection.patch
+patch -d bionic				-p1 -s -N --no-backup-if-mismatch < $ACE/non_pie_support.patch
